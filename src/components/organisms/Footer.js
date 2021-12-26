ed79a2b4a68bc49ce336239a9134cab0ac33283e
vadimgierko <85487884+vadimgierko@users.noreply.github.com>
2021-12-26 23:06:37 +0100

    Update Footer.js

--- src/components/organisms/Footer.js
+++ src/components/organisms/Footer.js
@@ -11,7 +11,7 @@ export default function Footer() {
       }}
     >
       <hr />
-      <p className="text-center mb-0 pb-3">&copy; 2021 Vadim Gierko</p>
+      <p className="text-center mb-0 pb-3">&copy; 2021 <a href="https://github.com/vadimgierko" target="_blank" style={{textDecoration: "none"}}>Vadim Gierko</a></p>
     </div>
   );
 }
