65a46073eb60f85a275f46308907acd7a5dcc52e
vadimgierko <85487884+vadimgierko@users.noreply.github.com>
2025-01-09 00:13:45 +0100

    init empty next.js ts project

--- src/components/RequireAuth/index.js
+++
@@ -1,8 +0,0 @@
-import { useSelector } from "react-redux";
-import { Navigate, useLocation } from "react-router-dom";
-
-export default function RequireAuth() {
-	const { pathname } = useLocation();
-
-	return <Navigate to="/signin" state={{ from: pathname }} replace />;
-}
