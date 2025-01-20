0b7b6b5600d673af834e3283158c16c1b1739b61
Vadim Gierko <85487884+vadimgierko@users.noreply.github.com>
2025-01-20 11:56:35 +0000

    change & remove prev SDK keys & use .env

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,17 +1,17 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getDatabase } from "firebase/database";
-// import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Add your Firebase credentials
 const firebaseConfig = {
-	apiKey: "AIzaSyBO2_8QOJb3muKvNWqxDfCzViDwNURiUJc",
-	authDomain: "linky-notes-47eef.firebaseapp.com",
-	databaseURL: "https://linky-notes-47eef-default-rtdb.firebaseio.com",
-	projectId: "linky-notes-47eef",
-	storageBucket: "linky-notes-47eef.appspot.com",
-	messagingSenderId: "1067601111242",
-	appId: "1:1067601111242:web:75f3f608656fe612ef6c33",
+	apiKey: process.env.REACT_APP_API_KEY,
+	authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+	projectId: process.env.REACT_APP_PROJECT_ID,
+	storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+	messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+	appId: process.env.REACT_APP_APP_ID,
 };
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
